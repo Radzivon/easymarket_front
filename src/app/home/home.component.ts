49ee372abfd1761eab02c1d4d3ad356d68c8fa7a
Dima-Iv <dimaivanenko99@gmail.com>
2020-05-04 22:04:33 +0300

    Upgraded the login, sign-up and home pages.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import {Component, OnInit} from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig]
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent implements OnInit {
+  showNavigationIndicators = false;
+  showNavigationArrows = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
